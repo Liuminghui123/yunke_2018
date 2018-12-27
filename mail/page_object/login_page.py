55610a78a65f05a94ea2398bc8d1596f943597e1
liumignhui <13439281553@163.com>
2018-12-27 11:47:57 +0800

    生成测试报告

---
+++ mail/page_object/login_page.py
@@ -0,0 +1,26 @@
+from selenium.webdriver.common.by import By
+from .base import Bsae
+import time
+
+class LoginPage(Bsae):
+    login_username_loc = (By.ID, 'uid')
+    login_pwd_loc = (By.ID, 'pwd')
+    login_button_loc = (By.ID, 'login_button')
+
+    def login_username(self, text):
+        return self.find_element_(*self.login_username_loc).send_keys(text)
+
+    def login_pwd(self, text):
+        return self.find_element_(*self.login_pwd_loc).send_keys(text)
+
+    def login_button(self):
+        return self.find_element_(*self.login_button_loc).click()
+
+
+    def login_action(self,username,password):
+        self.open()
+        self.login_username(username)
+        self.login_pwd(password)
+        self.login_button()
+
+
